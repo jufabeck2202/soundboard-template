be128fd9885bbe05d25e91f25eeb5cc757c02708
jufabeck2202 <ju-fa-beck@t-online.de>
2017-04-17 21:21:15 +0200

    finished soundboard template for ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,12 +14,12 @@ export class HomePage {
     //checks what platform it is runnion on
     if(/(android)/i.test(navigator.userAgent)) {
       this.admobId = {
-        banner: 'ca-app-pub-xxx/yyy',
+        banner: '',
         interstitial: 'ca-app-pub-jjj/kkk'
       };
     } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
       this.admobId = {
-        banner: 'ca-app-pub-ddd/sss',
+        banner: '',
         interstitial: 'ca-app-pub-ppp/zzz'
       };
     }
@@ -34,7 +34,7 @@ export class HomePage {
         AdMob.createBanner({
           adId: this.admobId.banner,
           autoShow: true,
-          isTesting:true
+          isTesting:false
         });
       }
     });
@@ -45,8 +45,8 @@ export class HomePage {
       if(AdMob) {
         AdMob.prepareInterstitial({
           adId: this.admobId.interstitial,
-          autoShow: true,
-          isTesting:true
+          autoShow: false,
+          isTesting:false
         });
       }
     });
@@ -73,18 +73,6 @@ export class HomePage {
   }
   items = [
     {"name":"two be continued","file":"assets/2to-be-continued.mp3"},
-    {"name":"dio WRYYY","file":"assets/dio-wryyy.mp3"},
-    {"name":"goodbye jojo","file":"assets/goodbye-jojo.mp3"},
-    {"name":"run","file":"assets/joestar-run.mp3"},
-    {"name":"Joooo Joooo","file":"assets/jojo.mp3"},
-    {"name":"OH NO","file":"assets/joseph-joestar-_oh-no_.mp3"},
-    {"name":"NICE","file":"assets/joseph-joestar-nice.mp3"},
-    {"name":"KONO DIO DA","file":"assets/kono-dio-da99.mp3"},
-    {"name":"JOJO INTRO","file":"assets/new-jjbatas-pb-intro.mp3"},
-    {"name":"OH SHIT","file":"assets/OH SHIT.mp3"},
-    {"name":"rero rero rero","file":"assets/rero-rero-rero.mp3"},
-    {"name":"SON OF A BITCH","file":"assets/SON OF A BITCH.mp3"},
-    {"name":"who the hell do you think i am","file":"assets/who-the-hell-do-you-think-i-am-.mp3"},
 
 
 
